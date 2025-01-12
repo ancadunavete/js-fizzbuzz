03ee0c47d7fc7534a3f37e14f421e8df005d3ba3
ancadunavete <anca.dnvt@gmail.com>
2025-01-12 18:55:20 +0100

    add: numbers

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 /*Fase prep:
 
-Fase elab
+Scrivi un programma che stampi i numeri da 1 a 100, ma
+per i multipli di 3 stampi “Fizz” al posto del numero
+per i multipli di 5 stampi Buzz
+per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz*/
 
-Fase output*/
+//Fase elab:
+
+
+
+//Fase output:
+
+for(let x = 1; x <= 100; x++) {
+    console.log(`here's a number: ${x}`)
+}
+
+
+
+
